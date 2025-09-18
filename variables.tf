4d82473d22dc11d8154ae665ea1925539e99a6d4
wprzechrzta <w.przechrzta@gmail.com>
2025-09-18 13:01:21 +0200

    feat: Disable resources

--- variables.tf
+++ variables.tf
@@ -1,33 +1,11 @@
-
 variable "domain" {
   type = string
-  default = "craigdunn.net"
-}
-
-
-variable "instance_name" {
-  type = string
-  default = "terraform-demo"
-}
-
-variable "ami_id" {
-  type = string
-  default = "ami-06d94a781b544c133"
+  default = "przechrzta.me"
 }
 
 variable "instance_type" {
   type = string
   default = "t2.micro"
-}
-
-variable "subnet" {
-  type = string
-  default = "subnet-01a4fdab38dabcf25"
-}
-
-variable "aws_keypair" {
-  type = string
-  default = "bolt"
 }
 
 variable "region" {
